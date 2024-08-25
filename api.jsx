ff21f7640c14418d8d98592bb775a73de9f787c5
MIDHUN V <106835220+mid1h3un@users.noreply.github.com>
2024-08-25 19:04:43 +0530

    Update api.jsx

--- api.jsx
+++ api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseUrl='https://api.openweathermap.org/data/2.5/weather?'
-const apiKey='cfbc2d2e3beaed834829aee5721d039b'
+const apiKey=import.meta.env.VITE_API_KEY
 const getWeatherData= async (cityname)=>{
   try{
     const {data}= await axios.get(baseUrl+`q=${cityname}&appid=${apiKey}`)
